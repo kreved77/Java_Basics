dec5e2659ae441a7f647cc27c100e116fe7c003b
kreved77 <81299409+kreved77@users.noreply.github.com>
2022-12-26 13:57:30 +0300

    Codewars.Com • HighestAndLowest (add FormatException)

--- src/CodewarsCom/HighestAndLowest.java
+++ src/CodewarsCom/HighestAndLowest.java
@@ -18,10 +18,12 @@ public class HighestAndLowest {
 //        String quote = "1 2 -3 4 5";
 //        String quote = "1 9 3 4 -5";
         String quote = "8 3 -5 42 -1 0 0 -9 4 7 4 -4";
+//        String quote = "8 3 -5 42aaaaa -1 0 0 -9 4 7 4 -4";
 //        String quote = "";
 //        String quote = null;
 
         System.out.println(highAndLow(quote));
+        System.out.println(highAndLowWithFormatException(quote));
         System.out.println(highAndLow2(quote));
         System.out.println(highAndLow3(quote));
     }
@@ -43,6 +45,24 @@ public class HighestAndLowest {
             min = value < min ? value : min;
         }
 //        return (max + " " + min);
+        return String.format("%s %s", max, min);
+    }
+
+    public static String highAndLowWithFormatException(String numbers) {
+        // Code here or
+        Integer max = Integer.MIN_VALUE;
+        Integer min = Integer.MAX_VALUE;
+        for (String e : numbers.split(" ")) {
+            try{
+                int value = Integer.parseInt(e);
+                max = value > max ? value : max;
+                min = value < min ? value : min;
+            }
+            catch (NumberFormatException ex){
+                System.out.println("NumberFormatException - not a valid input.");
+                ex.printStackTrace();
+            }
+        }
         return String.format("%s %s", max, min);
     }
 
