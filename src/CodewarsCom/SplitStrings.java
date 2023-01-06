396e65350e6b07e7bf513c251e4ef9c666823257
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-01-07 01:09:53 +0300

    Codewars.Com • SplitStrings (add junit.Test -> randomTests())

--- src/CodewarsCom/SplitStrings.java
+++ src/CodewarsCom/SplitStrings.java
@@ -3,6 +3,8 @@ package CodewarsCom;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 /*
 https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
@@ -12,9 +14,31 @@ public class SplitStrings {
     public static void main(String[] args) {
         testEvenString();
         testOddString();
+        randomTests();
     }
 
 // Test DATA
+    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz1234567890    ";
+    private static final Random rand = new Random();
+    private static char randomChar() {
+        return alphabet.charAt(rand.nextInt(alphabet.length()));
+    }
+
+    @Test
+    public static void randomTests() {
+        for (int trial = 1; trial <= 10; trial++) {
+            String[] expected = new String[rand.nextInt(21)];
+            for (int i = 0; i < expected.length; i++)
+                expected[i] = "" + randomChar() + randomChar();
+            if ( 0 < expected.length && rand.nextBoolean() )
+                expected[expected.length-1] = randomChar() + "_";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < expected.length; i++)
+                sb.append(expected[i]);
+            assertEquals(Arrays.toString(expected),
+                    Arrays.toString(solution(sb.toString().replace("_$", ""))));
+        }
+    }
     @Test
     public static void testEvenString() {
         String s = "abcdef";
@@ -30,6 +54,7 @@ public class SplitStrings {
         assertEquals("Should handle odd string","[Lo, ve, Pi, zz, a_]", Arrays.toString(solution(s1)));
     }
 
+// VER_1
     public static String[] solution(String s) {
         //Write your code here
         int n = s.length() / 2 + s.length() % 2;
@@ -37,6 +62,17 @@ public class SplitStrings {
         for (int i = 0; i < n; i++){
             sArr[i] = s.substring(2*i).length() >= 2 ? s.substring(2*i,2*i+2) : s.substring(2*i) + "_";
         }
+        System.out.println(Arrays.stream(sArr).collect(Collectors.joining()));
+        System.out.println(Arrays.stream(sArr).toList());
+//        System.out.println(Arrays.toString(sArr));
         return sArr;
     }
+
+// VER_2
+//    public static String[] solution(String s) {
+//        //Write your code here
+//        s = (s.length() % 2 == 0) ? s : s + "_";
+////        System.out.println(Arrays.stream(s.split("(?<=\\G.{2})")).collect(Collectors.toList()));
+//        return s.split("(?<=\\G.{2})");
+//    }
 }
